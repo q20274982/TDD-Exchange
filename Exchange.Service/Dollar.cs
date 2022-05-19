6f7c78ee76bbba0b0a91795a2a8fb28319511888
calvinchang <q20274982@gmail.com>
2022-05-19 23:48:56 +0800

    建立第一支測試 並通過測試

---
+++ Exchange.Service/Dollar.cs
@@ -0,0 +1,17 @@
+﻿namespace Exchange.Service
+{
+    public class Dollar
+    {
+        public int amount = 10;
+
+        public Dollar(int amount)
+        {
+
+        }
+
+        public void times(int multiplier)
+        {
+
+        }
+    }
+}
---
+++ Exchange.Test/MoneyTest.cs
@@ -0,0 +1,22 @@
+﻿using Exchange.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Exchange.Test
+{
+    public class MoneyTest
+    {
+        [Fact]
+        public void testMultiplication()
+        {
+            Dollar five = new Dollar(5);
+            five.times(2);
+
+            Assert.Equal(10, five.amount);
+        }
+    }
+}
