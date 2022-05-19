fa3bff5e1db85a27256b1afa23103cdaf731a72f
calvinchang <q20274982@gmail.com>
2022-05-20 00:48:56 +0800

    重構

--- Exchange.Test/MoneyTest.cs
+++ Exchange.Test/MoneyTest.cs
@@ -14,11 +14,8 @@ namespace Exchange.Test
         public void testMultiplication()
         {
             Dollar five = new Dollar(5);
-            Dollar product = five.times(2);
-            Assert.Equal(10, product.amount);
-
-            product = five.times(3);
-            Assert.Equal(15, product.amount);
+            Assert.Equal(new Dollar(10), five.times(2));
+            Assert.Equal(new Dollar(15), five.times(3));
         }
 
         [Fact]
